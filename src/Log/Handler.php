48e186072e4c8a4f844ee34765f1bbc5e5e9ab05
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-13 23:07:31 -0500

    Update Handler.php

--- src/Log/Handler.php
+++ src/Log/Handler.php
@@ -1,2 +1,19 @@
 <?php
 declare(strict_types=1);
+/**
+ * This file is a part of kitkat database management.
+ *
+ * @author Nicholas English (Owner/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/obozaxy/KitKat-Database-Management> Github repository.
+ * @license <https://github.com/obozaxy/KitKat-Database-Management/blob/master/LICENSE> Apache-2.0 license.
+ */
+
+namespace Obozaxy\Database\Log;
+
+/**
+ * Handler.
+ */
+class Handler
+{
+}
