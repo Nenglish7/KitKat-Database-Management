2b9d45928cde67e348c7316cbe70154f142c8638
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-16 02:44:16 -0500

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -3,17 +3,36 @@ declare(strict_types=1);
 /**
  * This file is a part of kitkat database management.
  *
- * @author Nicholas English (Owner/Developer) <nenglish0820@outlook.com>.
+ * @author Nicholas English <nenglish0820@outlook.com>.
  *
  * @link    <https://github.com/obozaxy/KitKat-Database-Management> Github repository.
  * @license <https://github.com/obozaxy/KitKat-Database-Management/blob/master/LICENSE> Apache-2.0 license.
  */
 
-namespace Obozaxy\Database\Log;
+namespace Obozaxy\KitKat;
 
 /**
- * Handler.
+ * Utils.
  */
-class Handler
+class Utils
 {
+    
+    /**
+     * Get the list of supported database drivers.
+     *
+     * @return array An array of the supported database
+     *               drivers.
+     *
+     * @codeCoverageIgnore
+     */
+    public static function getDrivers()
+    {
+        return [
+            'mysql',
+            'pgsql',
+            'oracle',
+            'sqlite'
+        ];
+    }
+  
 }
