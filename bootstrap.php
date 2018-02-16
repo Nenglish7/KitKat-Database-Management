b9b3a742abdc0c85ff9350b9f589c5cc792db94e
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-16 15:13:11 -0500

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,8 @@
 <?php
+/**
+ * This file is a part of KitKat Database Management.
+ *
+ * @link    <https://github.com/obozaxy/KitKat-Database-Management> Github Repository.
+ * @license <https://github.com/obozaxy/KitKat-Database-Management/blob/master/LICENSE> Apache-2.0
+ */
 require_once './vendor/autoload.php';
