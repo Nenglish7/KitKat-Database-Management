ae115d0e66d8341f54e4d02cd86f68b39065478a
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-18 17:26:17 -0500

    Delete bootstrap.php

--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * This file is a part of KitKat Database Management.
- *
- * @link    <https://github.com/obozaxy/KitKat-Database-Management> Github Repository.
- * @license <https://github.com/obozaxy/KitKat-Database-Management/blob/master/LICENSE> Apache-2.0
- */
-require_once './vendor/autoload.php';
