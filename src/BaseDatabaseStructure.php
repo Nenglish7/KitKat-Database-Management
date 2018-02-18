2861e1faa7064bf7ae9dd2a859dc1c5c69e72aaa
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-18 18:04:50 -0500

    Create BaseDatabaseStructure.php

---
+++ src/BaseDatabaseStructure.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is a part of KitKat Database Management.
+ *
+ * @link    <https://github.com/obozaxy/KitKat-Database-Management> Github Repository.
+ * @license <https://github.com/obozaxy/KitKat-Database-Management/blob/master/LICENSE> Apache-2.0
+ */
+namespace Obozaxy\KitKat;
+/**
+ * BaseDatabaseStructure.
+ */
+class BaseDatabaseStructure extends OutputController
+{
+}
